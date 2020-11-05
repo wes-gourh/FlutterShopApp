23bbdeb404acb75043c2f6d3a392d3ed74d29301
Wasim Gourh <38152653+wes-gourh@users.noreply.github.com>
2020-11-05 19:40:25 +0530

    added routes, changeNotifierProvider and did some theming

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/products_grids.dart';
+
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var scaffold = Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: ProductsGrid(),
+    );
+    return scaffold;
   }
 }
