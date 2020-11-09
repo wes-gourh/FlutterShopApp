f14628f63799e6c9001c3d5a9a9fe0139c35325e
Wasim Gourh <38152653+wes-gourh@users.noreply.github.com>
2020-11-09 21:40:14 +0530

    removed calling the constructor and added notifierprovider

--- lib/widgets/products_grids.dart
+++ lib/widgets/products_grids.dart
@@ -18,10 +18,13 @@ class ProductsGrid extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c) => products[i],
+        child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
     );
   }
