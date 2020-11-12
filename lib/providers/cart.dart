de30ba2ba536d256da25b87e8fd336f2280a59c8
Wasim Gourh <38152653+wes-gourh@users.noreply.github.com>
2020-11-12 18:47:56 +0530

    remove and clear functions

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -61,4 +61,14 @@ class Cart with ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
 }
