ea24765b1c2fb29aa1c47eccf59450309c30d41a
Wasim Gourh <38152653+wes-gourh@users.noreply.github.com>
2020-11-16 20:50:40 +0530

    product edit and delete option for users

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
   UserProductItem(
+    this.id,
     this.title,
     this.imageUrl,
   );
@@ -16,19 +22,27 @@ class UserProductItem extends StatelessWidget {
       leading: CircleAvatar(
         backgroundImage: NetworkImage(imageUrl),
       ),
-      trailing: Row(
-        children: [
-          IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {},
-            color: Theme.of(context).primaryColor,
-          ),
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () {},
-            color: Theme.of(context).errorColor,
-          )
-        ],
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
+              color: Theme.of(context).errorColor,
+            )
+          ],
+        ),
       ),
     );
   }
