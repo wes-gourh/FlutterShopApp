84f8f3227d0e13f690b7305e0ff8025021ab04f7
Wasim Gourh <38152653+wes-gourh@users.noreply.github.com>
2020-11-16 20:49:32 +0530

    changes for edit product

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+import '../screens/edit_product_screen.dart';
 
 class UserProductScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -11,21 +13,30 @@ class UserProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(
         title: const Text('Your Products'),
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           )
         ],
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemBuilder: (_, i) => UserProductItem(
-            productsData.items[i].title,
-            productsData.items[i].imageUrl,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].id,
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
           ),
           itemCount: productsData.items.length,
         ),
